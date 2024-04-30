ded23c0b65586e242069e22f0b556cbcbb6167cd
zhangyu <zhangyu2@tellhow.com>
2024-04-30 08:43:18 +0800

    commit

--- CS61A/prj/hog/hog.py
+++ CS61A/prj/hog/hog.py
@@ -146,6 +146,7 @@ def play(strategy0, strategy1, update,
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    
     # END PROBLEM 5
     return score0, score1
 

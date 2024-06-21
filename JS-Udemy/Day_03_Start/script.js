56175d6387e21c7edf8e41dc67e0e7bb0611c12b
zhangyu <zhangyu2@tellhow.com>
2024-06-21 16:11:45 +0800

    delete code

--- JS-Udemy/Day_03_Start/script.js
+++ JS-Udemy/Day_03_Start/script.js
@@ -1,4 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 23;
+/* const x = 23;
+console.log(1991); */
+
+/* const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 1; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(`the max values is ${max}`);
+};
+
+calcTempAmplitude([1, 2, 3, 4]); */
+
+// console.table(calcTempAmplitude([1, 2, 3]));
+
+const forecast = [17, 21, 23];
+
+const printForecast = function (fore) {
+  for (let i = 0; i < fore.length; i++) {
+    console.log(`${fore[i]} C in ${i + 1} days...`);
+  }
+};
+console.log(printForecast(forecast));
